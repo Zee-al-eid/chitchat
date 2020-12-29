04ea22b1120deeb54b0d380bbdf396600d822433
ZEE-AL-EID AHMED <realzeealeid@gmail.com>
2020-12-30 00:06:04 +0500

    updating

--- utils/messages.js
+++ utils/messages.js
@@ -1,14 +1,15 @@
 const moment = require('moment');
-var currentdate = new Date();
-var datetime = "Last Sync: " + currentdate.getDay() + "/" + currentdate.getMonth() 
-+ "/" + currentdate.getFullYear() + " @ " 
-+ currentdate.getHours() + ":" 
-+ currentdate.getMinutes() + ":" + currentdate.getSeconds();
+var dt = new Date();
+var hours = dt.getHours();
+var AmOrPm = hours >= 12 ? 'pm' : 'am';
+hours = (hours % 12) || 12;
+var minutes = dt.getMinutes() ;
+var finalTime = "Time  - " + hours + ":" + minutes + " " + AmOrPm;
 function formatMessage(username, text) {
   return {
     username,
     text,
-    time: datetime
+    time: finalTime
   };
 }
 
